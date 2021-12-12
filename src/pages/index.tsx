4da3905ef2362284754b75e5b56dcc7e15e8b02a
eryson <eryson.ktm@gmail.com>
2021-12-12 13:20:07 -0300

    feat: Created home page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +8,20 @@ export default function Home() {
         <title>Home | Ig News</title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acces to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
