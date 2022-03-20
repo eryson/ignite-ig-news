96041d0cac4636a83123015d0501ef99a44a9c99
Eryson Miranda <eryson.ktm@gmail.com>
2022-03-20 13:33:14 -0300

    fix: Fixed signIn function with stripe and with faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,24 +3,15 @@ import GithubProvider from "next-auth/providers/github";
 
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
-import { FaunaAdapter } from "@next-auth/fauna-adapter";
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      authorization: {
-        params: {
-          scope: "read:user",
-        },
-      },
+      authorization: "https://github.com/login/oauth/authorize?scope=read:user",
     }),
   ],
-
-  adapter: FaunaAdapter(fauna),
-
-  secret: process.env.NEXT_AUTH_SECRET,
 
   callbacks: {
     async signIn({ user }) {
@@ -30,10 +21,12 @@ export default NextAuth({
         await fauna.query(
           q.If(
             q.Not(
-              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
             ),
             q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
         );
 
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -2,6 +2,5 @@ import { Client } from "faunadb";
 
 export const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
-  scheme: "https",
   domain: "db.us.fauna.com",
 });
