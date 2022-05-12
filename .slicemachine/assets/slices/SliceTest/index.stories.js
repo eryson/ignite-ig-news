b739d034487a73f599429705de6c8ecf3683fb42
Eryson Miranda <eryson.ktm@gmail.com>
2022-05-12 13:14:28 -0300

    feat: Created slicemachine files

---
+++ .slicemachine/assets/slices/SliceTest/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/SliceTest';
+
+export default {
+  title: 'slices/SliceTest'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"slice_test","items":[],"primary":{"title":[{"type":"heading1","text":"Iterate end-to-end eyeballs","spans":[]}],"description":[{"type":"paragraph","text":"Voluptate cupidatat eu elit duis non sint adipisicing aliqua reprehenderit. Nostrud velit dolore reprehenderit consectetur quis consectetur id consequat ad proident sunt laborum velit incididunt aliquip.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- src/pages/slice-simulator.jsx
+++ src/pages/slice-simulator.jsx
@@ -1,14 +1,18 @@
 import { SliceSimulator } from "@prismicio/slice-simulator-react";
 import { SliceZone } from "@prismicio/react";
 
+import { components } from "../../slices/index";
 import state from "../../.slicemachine/libraries-state.json";
-import { components } from "../slices";
 
-const SliceSimulatorPage = () => (
-  <SliceSimulator
-    sliceZone={(props) => <SliceZone {...props} components={components} />}
-    state={state}
-  />
-);
+const SliceSimulatorPage = () => {
+  return (
+    <SliceSimulator
+      sliceZone={({ slices }) => (
+        <SliceZone slices={slices} components={components} />
+      )}
+      state={state}
+    />
+  );
+};
 
 export default SliceSimulatorPage;
